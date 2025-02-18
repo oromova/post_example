ce1bfb8f6cf860e1d5aad575bdf3c9f82ee70dbb
Yulduz_Oromova <mahmurbekovna@gmail.com>
2025-02-18 15:36:22 +0500

    Location

--- src/Pages/Locations.jsx
+++ src/Pages/Locations.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Locations() {
   const [locations, setLocations] = useState([])
@@ -20,18 +20,19 @@ function Locations() {
   getLocation()
  }, []);
 
-  return (
-    <div>
-      {
-        locations.map(location => (
-          <div key={location.id}>
-            <img src={`${imageUrl}/${location.image_src}`} alt="location" />
-          </div>
+ return (
+  <div className='grid grid-cols-3 gap-[20px] mt-[50px]'>
+    {
+      locations.map((location) => (
+        <div className="bg-white shadow-md rounded-lg p-4 flex flex-col items-center justify-center transition transform hover:scale-105" key={location.id}>
+          <img src={`${imageUrl}/${location.image_src}`} alt='location' className="object-contain" />
+          <p className="mt-2 font-semibold text-gray-700">{location.name}</p>
+        </div>
+      ))
+    }
+  </div>
+);
 
-        ))
-      }
-    </div>
-  )
 }
 
 export default Locations
